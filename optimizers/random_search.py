d21d75eacbc5249e94055c5a99235a305979c191
Sohambasu07 <soham.basu07@gmail.com>
2023-11-24 14:59:39 +0530

    Added compatibility check variables for Optimizers and minor restructuring

--- optimizers/random_search.py
+++ optimizers/random_search.py
@@ -1,56 +1,64 @@
+import os
 from ConfigSpace import ConfigurationSpace, Configuration
-from typing import ClassVar
 from pathlib import Path
 from hpo_glue.glu import Optimizer, Query, Result, Config, ProblemStatement
 import random
 
 class RandomSearch(Optimizer):
-    name: ClassVar[str] = "RandomSearch"
+    name = "RandomSearch"
+    supports_multifidelity = True
+    supports_tabular = True
 
     def __init__(self, 
-                 ProblemStatement: ProblemStatement,
+                 problem_statement: ProblemStatement,
                  working_directory: Path,
                  seed: int | None = None):
         """ Create a Random Search Optimizer instance for a given problem statement """
 
-        self.problem = ProblemStatement
-        self.config_space: ConfigurationSpace = ProblemStatement.config_space
-        self.fidelity_space: list[int] | list[float] = ProblemStatement.fidelity_space
-        self.objectives = ProblemStatement.result_keys
+        if isinstance(problem_statement.result_keys, list):
+            raise NotImplementedError("# TODO: Implement multiobjective for RandomSearch")
+
+        self.problem_statement = problem_statement
+        self.config_space: ConfigurationSpace = self.problem_statement.config_space
+        self.fidelity_space: list[int] | list[float] = self.problem_statement.fidelity_space
+        self.objectives = self.problem_statement.result_keys
         self.seed = seed
-        self.best_cost = 0.0
-        self.incumbent = None
         self.rng = random.Random(seed)
-        self.minimize = self.problem.minimize
+        self.minimize = self.problem_statement.minimize
+        self.is_multifidelity = self.problem_statement.is_multifidelity
+        self.is_manyfidelity = self.problem_statement.is_manyfidelity
+        self.is_tabular = self.problem_statement.is_tabular
+        self.is_multiobjective = self.problem_statement.is_multiobjective
+
+        if os.path.exists(working_directory) is False:
+            os.makedirs(working_directory)
         
     def get_config(self, num_configs: int) -> Configuration | list[Configuration]:
         """ Sample a random config or a list of configs from the configuration space """
 
         sample_seed = self.rng.randint(0, 2**31-1)  # Big number so we can sample 2**31-1 possible configs
-        self.config_space.seed(self.seed)
+        print(sample_seed)
+        self.config_space.seed(sample_seed)
         config = self.config_space.sample_configuration(num_configs)
         return config
-    
-    def get_incumbent(self) -> tuple[Configuration, float]:
-        print(type(self.incumbent))
-        return (Configuration(configuration_space=self.config_space, 
-                              values = self.incumbent),
-                self.best_cost)
-    
+        
     def ask(self,   
             config_id: str | None = None) -> Query:
         """ Ask the optimizer for a new config to evaluate """
 
+        fidelity = None
+        # Randomly sampling from fidelity space for multifidelity
+        if self.is_multifidelity:
+            fidelity = self.rng.choice(self.fidelity_space)
+
         # We are dealing with a tabular benchmark
-        if isinstance(self.config_space, list):
-            config = random.choice(self.config_space)
-            fidelity = random.choice(self.fidelity_space)
+        if self.is_tabular:
+            config = self.rng.choice(self.config_space)
             return Query(config, fidelity)
         
         # We are dealing with a surrogate benchmark
         else:
             config = self.get_config(1)
-            fidelity = random.choice(self.fidelity_space)
             # NOTE: I'm not sure how to deal with the `id` here...
             # There's no real order as the order in which you get configs every seed
             # will differ.
@@ -60,9 +68,6 @@ class RandomSearch(Optimizer):
     def tell(self, result: Result) -> None:
         """ Tell the optimizer the result of the query """
 
-        cost = result.result[self.problem.result_keys]
+        cost = result.result[self.problem_statement.result_keys]
         if self.minimize is False:
-            cost = 1 - cost
-        if self.best_cost == 0.0 or cost < self.best_cost:
-            self.best_cost = cost
-            self.incumbent = result.query.config.values
+            cost = -cost
--- optimizers/smac.py
+++ optimizers/smac.py
@@ -1,8 +1,6 @@
 import os
-from ConfigSpace import ConfigurationSpace, Configuration
-from typing import ClassVar
+from ConfigSpace import ConfigurationSpace
 from pathlib import Path
-import random
 from hpo_glue.glu import Optimizer, Query, Result, Config, ProblemStatement
 from smac import (HyperparameterOptimizationFacade as HPOFacade, 
                   MultiFidelityFacade as MFFacade,
@@ -10,98 +8,113 @@ from smac import (HyperparameterOptimizationFacade as HPOFacade,
 from smac.runhistory.dataclasses import TrialInfo, TrialValue
 
 class SMAC_Optimizer(Optimizer):
-    name: ClassVar[str] = "SMAC"
+    name = "SMAC"
+    supports_multifidelity = True
+
+
     def __init__(self,
-                 ProblemStatement: ProblemStatement,
+                 problem_statement: ProblemStatement,
                  working_directory: Path,
                  seed: int | None = None):
         """ Create a SMAC Optimizer instance for a given problem statement """
+
+        if isinstance(problem_statement.result_keys, list):
+            raise NotImplementedError("# TODO: Implement multiobjective for SMAC")
         
-        self.problem = ProblemStatement
-        self.config_space: ConfigurationSpace = ProblemStatement.config_space
-        self.fidelity_space: list[int] | list[float] = ProblemStatement.fidelity_space
-        self.objectives: str | list[str] = ProblemStatement.result_keys
+        if isinstance(problem_statement.fidelity_keys, list):
+            raise NotImplementedError("# TODO: Manyfidelity not yet implemented for SMAC!")
+        
+        self.problem_statement = problem_statement
+        self.config_space: ConfigurationSpace = self.problem_statement.config_space
+        self.fidelity_space: list[int] | list[float] | None = self.problem_statement.fidelity_space
+        self.objectives: str | list[str] = self.problem_statement.result_keys
+        if seed is None:
+            seed = -1
         self.seed = seed
-        self.rng = random.Random(seed)
-        self.smac_info : TrialInfo = None #No parallel support
-        self.smac_val : TrialValue = None #No parallel support
+        self.smac_info : TrialInfo | None = None #No parallel support
+        self.smac_val : TrialValue | None = None #No parallel support
 
 
         if os.path.exists(working_directory) is False:
             os.makedirs(working_directory)
 
+        min_budget = None
+        max_budget = None
+
+        if self.fidelity_space is not None:
+            min_budget = self.fidelity_space[0]
+            max_budget = self.fidelity_space[-1]
+
+        n_trials = 1
+        if self.problem_statement.budget_type == "n_trials":
+            n_trials = self.problem_statement.budget
+
         self.scenario = Scenario(
             configspace=self.config_space,
             deterministic=False,
             objectives=self.objectives,
-            n_trials=ProblemStatement.n_trials,
+            n_trials=n_trials,
             output_directory=working_directory,
+            min_budget=min_budget,
+            max_budget=max_budget,
+            seed=self.seed
         )
 
-        self.is_multifidelity = self.problem.is_multifidelity
-        self.is_manyfidelity = self.problem.is_manyfidelity
-        self.is_tabular = self.problem.is_tabular
-        self.is_multiobjective = self.problem.is_multiobjective
-        self.minimize = self.problem.minimize
+        self.is_multifidelity = self.problem_statement.is_multifidelity
+        self.is_manyfidelity = self.problem_statement.is_manyfidelity
+        
+        self.is_tabular = self.problem_statement.is_tabular
+        if self.is_tabular:
+            raise ValueError("SMAC does not support tabular benchmarks!")
+        
+        self.is_multiobjective = self.problem_statement.is_multiobjective
+        self.minimize = self.problem_statement.minimize
 
+        if self.is_multifidelity:
+            self.facade = MFFacade
+        else:
+            self.facade = HPOFacade
 
-        facade = self.get_facade()
-        self.intensifier = facade.get_intensifier(
+        self.intensifier = self.facade.get_intensifier(
             self.scenario,
-            max_config_calls=1,
         )
-        self.smac = facade(scenario=self.scenario,
-                         target_function=lambda seed: self.seed,
+
+        self.smac = self.facade(scenario=self.scenario,
+                         target_function=lambda seed, budget: None,
                          intensifier=self.intensifier,
                          overwrite=True)
-        
-        self.best_cost = 0.0
-        
-    def get_incumbent(self) -> tuple[Configuration | list[Configuration], 
-                                     float | list[float]]:
-        return self.intensifier.get_incumbent, self.best_cost
-
-
-    def get_facade(self):
-        if self.is_multifidelity:
-            return MFFacade
-        else:
-            return HPOFacade
     
     def ask(self,
             config_id: str | None =  None) -> Query:
         """ Ask SMAC for a new config to evaluate """
         
         self.smac_info = self.smac.ask()
-        config_id = self.intensifier.runhistory.get_config_id(self.smac_info.config) #For now using SMAC's own config_id
-        config = self.smac_info.config.get_dictionary()
+        config = self.smac_info.config
+        budget = self.smac_info.budget
+        instance = self.smac_info.instance
+        seed = self.smac_info.seed
         fidelity = None
-        if self.problem.fidelity_keys is None:
-            if isinstance(self.fidelity_space, list):
-                fidelity = self.fidelity_space[-1]
-            elif isinstance(self.fidelity_space, ConfigurationSpace):
-                #ideally get the highest fidelity; haven't figured out how to from ConfigSpace
-                fidelity = self.fidelity_space.sample_configuration(1)
-            else:
-                raise ValueError("Fidelity space must be a list or ConfigSpace!")
 
-        # else:
-        #     fidelity = #Multifidelity setting for SMAC
-        return Query(Config(config_id, config), fidelity=fidelity)
+        if self.smac_info.budget is not None:
+            fidelity = budget
+
+        _config_id = self.intensifier.runhistory.config_ids[config] #For now using SMAC's own config_id
+
+        config = Config(
+            id=f"{_config_id=}_{seed=}_{instance=}",
+            values=config.get_dictionary(),
+            )
+        
+        if isinstance(fidelity, float):
+            fidelity = round(fidelity)
+
+        return Query(config=config, fidelity=fidelity)
     
     def tell(self,
              result: Result) -> None:
         """ Tell SMAC the result of the query """
-        cost = result.result[self.objectives]   #Not considering Multifidelity for now
+        cost = result.result[self.objectives]   #Not considering Multiobjective for now
         if self.minimize is False:
-            cost = 1.0 - cost
-        # if self.best_cost == [] or cost < self.best_cost[-1]:
-        #     self.best_cost.append(cost)
-        #     self.incumbents.append(self.smac_info.config)
-        # else:
-        #     self.best_cost.append(self.best_cost[-1])
-        #     self.incumbents.append(self.incumbents[-1])
-        if self.best_cost == 0.0 or cost < self.best_cost:
-            self.best_cost = cost
+            cost = -cost
         self.smac_val = TrialValue(cost = cost, time = 0.0)
         self.smac.tell(self.smac_info, self.smac_val)
