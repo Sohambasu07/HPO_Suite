7bed22f80a5baaeb5a6ab720ad86e7b9835eb723
Sohambasu07 <soham.basu07@gmail.com>
2024-01-06 22:51:06 +0530

    Added get_all_benchmarks()

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -1,16 +1,27 @@
-from hpo_glue.glu import Benchmark
+from __future__ import annotations
+
+from typing import Dict, TYPE_CHECKING
+if TYPE_CHECKING:
+    from hpo_glue.glu import Benchmark
 from benchmarks.mfp_bench import lcbench_tabular, yahpo_surrogate_benchmark
+
+BENCHMARK_FACTORIES = {
+        "lcbench-tabular": lcbench_tabular,
+        "yahpo": yahpo_surrogate_benchmark,
+    }
 
 def get_benchmark(name: str, **kwargs) -> Benchmark:
     """Entry point of the repo to get a benchmark."""
     # This will probably look like a big if statement
     # or do some introspection on Benchmark.__subclasses__
-    BENCHMARK_FACTORIES = {
-        "lcbench-tabular": lcbench_tabular,
-        "yahpo": yahpo_surrogate_benchmark,
-    }
+    
     factory = BENCHMARK_FACTORIES.get(name)
     if factory is None:
         raise ValueError(f"Unknown benchmark {name}")
 
     return factory(**kwargs)
+
+def get_all_benchmarks() -> Dict[str, Benchmark]:
+    """Get all benchmarks."""
+
+    return BENCHMARK_FACTORIES
