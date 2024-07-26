Fixed Precision issues

src/hpo_glue/config.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 from typing import Any
 import numpy as np
 
+PRECISION = 12
 
 @dataclass
 class Config:
@@ -18,7 +19,9 @@ class Config:
     In the case this config was deserialized, it will likely be `None`.
     """
 
-    def to_tuple(self, precision) -> tuple:
+    def to_tuple(self, precision: int | None = None) -> tuple:
+        if precision is None:
+            precision = PRECISION
         return tuple(
             self.set_precision(
                 self.values, 
@@ -26,7 +29,8 @@ class Config:
             ).values()
         )
 
-    def set_precision(self, values: dict, precision: int = 12) -> None:
+    def set_precision(self, values: dict, precision: int) -> None:
         for key, value in values.items():
             if isinstance(value, float):
-                self.values[key] = np.round(value, precision)
+                values[key] = np.round(value, precision)
+        return values

src/hpo_glue/result.py
@@ -28,7 +28,9 @@ class Result:
     values: dict[str, Any]
     """Everything returned by the benchmark for a given query at the fideltiy."""
 
-    values_prec: dict[tuple, dict[str, list[int | float]]]
+    # values_prec: dict[tuple, dict[str, list[int | float]]]
+    """The values of the configuration as a tuple along with all the fidelities 
+    queried for that particular config."""
 
     budget_cost: float = np.nan
     """The amount of budget used to generate this result."""

src/hpo_glue/run.py
@@ -41,6 +41,8 @@ T = TypeVar("T", bound=Hashable)
 
 logger = logging.getLogger(__name__)
 
+GLOBAL_SEED = 42
+
 
 def _try_delete_if_exists(path: Path) -> None:
     if path.exists():
@@ -409,6 +411,16 @@ class Run:
                 raise ValueError(f"Unknown state {state}")
 
     @classmethod
+    def generate_seeds(
+        cls,
+        num_seeds: int,
+    ):
+        """Generate a set of seeds using a Global Seed."""
+        cls._rng = np.random.default_rng(GLOBAL_SEED)
+        seeds = cls._rng.integers(0, 2 ** 32 - 1, size=num_seeds)
+        return seeds
+
+    @classmethod
     def generate(  # noqa: PLR0913
         cls,
         optimizers: (
@@ -421,7 +433,8 @@ class Run:
         *,
         expdir: Path | str = DEFAULT_RELATIVE_EXP_DIR,
         budget: BudgetType | int,
-        seeds: Iterable[int],
+        # seeds: Iterable[int],
+        num_seeds: int,
         fidelities: int = 0,
         objectives: int = 1,
         costs: int = 0,
@@ -455,6 +468,11 @@ class Run:
                 * "raise": Raise an error.
                 * "ignore": Ignore the error and continue.
         """
+        
+        # Generate seeds
+        seeds = cls.generate_seeds(num_seeds).tolist()
+        print(seeds)
+
         _benchmarks: list[BenchmarkDescription] = []
         match benchmarks:
             case BenchmarkDescription():